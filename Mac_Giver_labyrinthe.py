2b25507268a81aa95563554f76030440483d90be
charlinox <charles.casassus@gmail.com>
2018-12-28 16:48:03 +0100

    Created Level_1_Liste_de_liste.json et *.txt

---
+++ Mac_Giver_labyrinthe.py
@@ -0,0 +1,13 @@
+
+# -*- coding: Utf-8 -*
+
+"""
+Jeu Mac Gyver Labyrinthe
+Jeu dans lequel on doit déplacer Mac Gyver afin de ramasser 3 objets (une aiguille, un petit tube en plastique et de l'éther) dispersé à travers un labyrinthe avant de gagner en affrontant le gardien.
+
+Script Python
+Fichiers : à compléter
+"""
+
+
+
