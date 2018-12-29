bbd26b0d05b684091e449f511dc0b891e5299bb0
charlinox <charles.casassus@gmail.com>
2018-12-29 09:40:20 +0100

    Completed list_of_list.py

--- Mac_Giver_maze_ List_ of_list.py
+++ Mac_Giver_maze_ List_ of_list.py
@@ -10,4 +10,22 @@ Fichiers : à compléter
 """
 
 
+class Niveau:
+		
+    def __init__(self, fichier):
+		self.file = LEVEL_FILL # LEVEL_FILL est une constante définie dans constantes.py #
+        #Faut il en faire une property pour l'utiliser dans la fonction suivante ?#
 
+
+    def generer(self):
+		
+		with open(self.fichier, "r") as fichier:
+			level = []
+			
+			for line in fichier:
+				line_level = []				
+				for sprite in line:					
+					if sprite != '\n':						
+						line_level.append(sprite)
+				level.append(line_level)
+			self.structure = level
