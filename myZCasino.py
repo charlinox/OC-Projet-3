2dc76639e7c8ad5ebb6d2388dc2dd8301b2d116f
charlinox <charles.casassus@gmail.com>
2018-12-30 20:42:47 +0100

    myZCasino.py revision

--- myZCasino.py
+++ myZCasino.py
@@ -1,18 +1,21 @@
 # -*-coding:utf-8 -*
-import os
+
 import random
 import math
 
 cagnote = 1000
+
 print("Vous avez", cagnote, "$ au départ.")
 
 rejouer = "o"
 while rejouer == "o": 
     nombre_mise = 1
-    while !nombre_mise in range(50):
-        nombre_mise = input("Tapez le nombre sur lequel vous voulez miser (entre 0 et 49) : ")
+    while nombre_mise not in range(50):
+        nombre_mise = int(input("Tapez le nombre sur lequel vous voulez miser (entre 0 et 49) : "))
     
     mise = input("Quelle somme allez vous miser ?")
+    
+    
     if mise >= cagnote
         mise = cagnote
         print("Tapie !")
@@ -21,9 +24,9 @@ while rejouer == "o":
 
     if nombre_mise == roulette:
         cagnote += math.ceil(mise * 3)
-        print("Vous avez gagner. Recevez trois fois la mise.")S
+        print("Vous avez gagner. Recevez trois fois la mise.")
     elif nombre_mise % 2 == roulette % 2:
-        cagnote += math.ceil((mise /2))
+        cagnote += math.ceil(mise /2)
         print("Vous avez la bonne couleur. Recevez 50% de la mise")
     else
         cagnote -= mise
@@ -34,6 +37,5 @@ while rejouer == "o":
         print("Vous êtes ruiné. Adios")
     else
         rejouer = input("Votre cagnote est de "+ cagnote +". Voulez vous rejouer ? o/n")
-    
-os.system("pause")
+
 
