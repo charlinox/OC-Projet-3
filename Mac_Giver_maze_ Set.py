424fdd2317ab737f9c5fc4ed38f15daf369b402f
charlinox <charles.casassus@gmail.com>
2019-01-04 15:12:51 +0100

    travail sur set 2.py

--- Mac_Giver_maze_ Set.py
+++ Mac_Giver_maze_ Set.py
@@ -8,29 +8,25 @@ class Niveau:
 		self.fichier = fichier
 		self.couloirs = {}
 		        
-    def lecture(self, fichier):
+    def lire_fichier(self, fichier):
         """Méthode permettant de lire le fichier en créant un set contenant les tuples des coordonnées des espaces vides (les couloirs)"""
 
         with open(fichier) as f:
             data = json.load(f)
-            for entree in data:
-                couloirs.append(entree)
-    
+            self.couloirs = set(data)    
     
         
     def afficher(self):
 
 class Outils:
     """ Gestion des outils représenté par un set de trois tuples contenant les coordoonées des trois objets. """
+    def __init__(self):
+        self.emplacement_outils = None
     
-    def placerObjets(self):
+    def placerObjets(self, niveau):
         """ Création d'un set contenant les coordoonées des trois outils à ramasser. """
-        emplacementOutils = {} 
-        couloirsDispo = couloirs # Copie pour manipulation #
-        
-        if couloirsDispo.get(posDepart or posArrivee):
-            couloirsDispo.pop(posDepart or posArrivee) # Sauf départ et arrivée
-            emplacementOutils = random.sample(couloirsDispo, 3)
+        couloirsDispo = niveau.couloirs # Copie pour manipulation #
+        self.emplacement_outils = set(random.sample(couloirsDispo - {POS_DEPART, POS_ARRIVEE}, 3))
         
 				
 		
@@ -38,15 +34,14 @@ class Personnage:
     
     def __init__(self):
         self.compteurObjets = 0
-        self.posX = posDepart[1]
-        self.posY = posDepart[2]
+        self.posX, self.posY = posDepart
         
     def deplacer(self, deplacement):
-        switch (deplacement) {
-            case d: self.posX += 1;       
-            case g: self.posX -= 1;
-            case h: self.posY += 1;
-            case b: self.posY -= 1;
+        actions = {
+            "d": lambda: (pos_x + 1, pos_y),
+            "g": lambda: (pos_x - 1, pos_y),
+            "h": lambda: (pos_x, pos_y - 1),
+            "b": lambda: (pos_x, pos_y + 1),
         }
         
         
