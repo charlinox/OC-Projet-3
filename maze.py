dcf43b7e0f940771433dbd4e47498d046af836e8
charlinox <charles.casassus@gmail.com>
2019-01-17 12:09:57 +0100

    def main

--- maze.py
+++ maze.py
@@ -5,18 +5,19 @@ import random
 
 class Niveau:
 		
-    def __init__(self, fichier): # Pas besoin du paramètre "fichier" ? #
-        self.fichier = fichier # du coup j'au intégrer le paramètre.
+    def __init__(self, fichier):
+        self.fichier = fichier
         self.couloirs = set()
         self._depart = set()
         self.arrivee = set()
+        self.lire_fichier()
         self.personnage = Personnage(self) 
-        self.outils = ElementsFixes(self)
-        #self.outils.placer()
+        self.outils = ElementsFixes()
+        
 		        
-    def lire_fichier(self, fichier):
+    def lire_fichier(self):
         """Méthode permettant de lire le fichier en créant un set contenant les tuples des coordonnées des espaces vides (les couloirs)"""
-        with open(fichier) as f:
+        with open(self.fichier) as f:
 
             for i, ligne in enumerate(f):
                 for j, col in enumerate(ligne):
@@ -28,23 +29,23 @@ class Niveau:
                         self.arrivee.add((i, j))
 
     
-    def afficher(self, couloirs, outils):
+    def afficher(self):
         maze = ""
-        for index_col in range(15):
-            for index_line in range(15):
+        for index_line in range(15):
+            for index_col in range(15):
                 if (index_line, index_col) in self.couloirs:
                     # print (self._outils.emplacement_outils)
-                    if (index_line, index_col) in self.outils.emplacement_outils:
-                        maze += "O" # affichage de trois outils génériques #
-                    elif (index_line, index_col) == self.personnage.obtenir_position():
+                    if (index_line, index_col) == self.personnage.obtenir_position():
                         maze += "P" # affichage du personnage #
+                    elif (index_line, index_col) in self.outils.emplacement_outils:
+                        maze += "O" # affichage de trois outils génériques #
                     else:
                         maze += " " # affichage d'un couloir vide #
                 else:
                     maze += "X" # affichage d'un mur #
             maze += "\n"
         
-        print (maze)
+        print(maze)
 
     def est_permis(self, position):
         return  position in self._couloirs
@@ -52,8 +53,12 @@ class Niveau:
     @property
     def pos_depart(self):
         print (self._depart)
-        return list(self._depart)
+        return list(self._depart)[0]
     
+    @property
+    def pos_arrivee(self):
+        print (self.arrivee)
+        return list(self.arrivee)[0]
 
 class ElementsFixes:
     """ Gestion des outils représenté par un set de trois tuples contenant les coordoonées des trois objets. """
@@ -64,7 +69,12 @@ class ElementsFixes:
     def placer(self, niveau):
         """ Création d'un set contenant les coordoonées des trois outils à ramasser. """
         couloirs = niveau #.couloirs  ? # Copie pour manipulation.  #
-        self.emplacement_outils = set(random.sample(couloirs - {POS_DEPART, POS_ARRIVEE}, 3))
+        self.emplacement_outils = set(
+            random.sample(
+                niveau.couloirs - {niveau.pos_depart, niveau.pos_arrivee}, 
+                3
+            )
+        )
         
     def ramasser(self, pos_actuelle):
         if pos_actuelle in self.emplacement_outils:
@@ -96,9 +106,9 @@ class Personnage:
         
     
     def combat(self, pos_actuelle):
-        if pos_actuelle == POS_ARRIVEE and mac_gyver.compteurObjets == 3:
+        if pos_actuelle == self.pos_arrivee and mac_gyver.compteurObjets == 3:
             print("Vous avez tué le gardien. Vous êtes libre.")
-        elif pos_actuelle == POS_ARRIVEE and mac_gyver.compteurObjets < 3:
+        elif pos_actuelle == self.pos_arrivee and mac_gyver.compteurObjets < 3:
             print("Vous êtes mort !")
         continuer = 0
         return (continuer)
@@ -109,23 +119,22 @@ class Personnage:
 def main():
     
     niveau1 = Niveau("level_1")
-    mac_gyver = niveau1.personnage
-    elements_fixes = niveau1.outils
-    
-    niveau1.lire_fichier("level_1")
-    elements_fixes.placer(niveau1.couloirs)
-    niveau1.afficher(niveau1.couloirs)
+    elements_fixes.placer(niveau1)
+    niveau1.afficher()
     
     #BOUCLE PRINCIPALE
     continuer = 1 # et pourquoi pas plutot "true" ? #
     while continuer:
         
-        deplacement = lower(input("Veuillez entrer une lettre pour déplacer Mac Gyver (d, q, z, s) ou 'p' pour sortir :"))
+        deplacement = input(
+            "Veuillez entrer une lettre pour déplacer Mac Gyver (d, q, z, s) "
+            "ou 'p' pour sortir : "
+        ).lower()
         
         pos_actuelle = mac_gyver.deplacer(deplacement)       
         elements_fixes.ramasser(pos_actuelle)
         mac_gyver.combat(pos_actuelle)
         
-        niveau1.afficher(niveau1.couloirs)
+        niveau1.afficher()
 
 main()
